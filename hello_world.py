21aaa99dba090f59d6625ebc880e1028194d02ae
Melissa Cayford <melissacayford@hotmail.co.uk>
2024-03-30 13:23:38 +0000

    Changed statement to git is awesome

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+#asking the user for their name and storing it as name
+name = input("what is your name?")
+
+#printing out the user's name
+print(name)
+
+#asking the user's age and storing it as age
+age = input("what is your age?")
+
+#printing out the user's age
+print(age)
+
+#printing out "Hello World"
+print("Hello World!")
